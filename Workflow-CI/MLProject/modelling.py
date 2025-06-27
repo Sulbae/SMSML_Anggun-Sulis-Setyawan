002e0c00e577bf296faa3aed0e737fdb0c5c224f
Sulbae <assulis500@gmail.com>
2025-06-27 19:55:26 +0700

    add file .py pada MLproject workflow

--- Workflow-CI/MLProject/modelling.py
+++ Workflow-CI/MLProject/modelling.py
@@ -4,13 +4,16 @@ from sklearn.model_selection import train_test_split
 from sklearn.ensemble import RandomForestClassifier
 import random
 import numpy as np
+import os
+import warnings
+import sys
 
-mlflow.set_tracking_uri("http://127.0.0.1:5000/")
+if __name__ == "__main__":
+    warnings.filterwarnings("ignore")
+    np.random.seed(40)
 
-# Membuat file ML Flow Experiment baru
-mlflow.set_experiment("Random Forest Model for Water Potability")
-
-data = pd.read_csv("water_potability_preprocessing.csv")
+    file_path = sys.argv[3] if len(sys.argv) > 3 else os.path.join(os.path.dirname(os.path.abspath(__file__)), "water_potability_preprocessing.csv")
+    data = pd.read_csv(file_path)
 
 # Fungsi split data
 def split_data(df, target='Potability', test_size=0.25, random_state=42):
@@ -23,20 +26,19 @@ X_train, X_test, y_train, y_test = split_data(data, target='Potability', test_si
 # Menyimpan snippet atau sample input
 input_example = X_train[0:5]
 
-# Random Forest Modelling
+n_estimators = int(sys.argv[1]) if len(sys.argv) > 1 else 505
+max_depth = int(sys.argv[2]) if len(sys.argv) > 2 else 37
+ 
 with mlflow.start_run():
-    
-    # Log parameters
-    n_estimators = 100
-    max_depth = 5
-    mlflow.autolog()
-
-    # Train model
     model = RandomForestClassifier(n_estimators=n_estimators, max_depth=max_depth)
+    model.fit(X_train, y_train)
+ 
+    y_pred = model.predict(X_test)
+ 
     mlflow.sklearn.log_model(
-        sk_model=model,
-        artifact_path="model",
-        input_example=input_example
+    sk_model=model,
+    artifact_path="model",
+    input_example=input_example
     )
     model.fit(X_train, y_train)
 
