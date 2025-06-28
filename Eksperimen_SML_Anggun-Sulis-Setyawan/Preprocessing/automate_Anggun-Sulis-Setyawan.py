095c7f918c0f03fdb33d5aebe9152a9e447db2c3
Sulbae <assulis500@gmail.com>
2025-06-28 21:31:16 +0700

    update

--- Eksperimen_SML_Anggun-Sulis-Setyawan/Preprocessing/automate_Anggun-Sulis-Setyawan.py
+++ Eksperimen_SML_Anggun-Sulis-Setyawan/Preprocessing/automate_Anggun-Sulis-Setyawan.py
@@ -1,28 +1,24 @@
 import numpy as np
 import pandas as pd
-from joblib import dump, load
+from joblib import dump
 from sklearn.pipeline import Pipeline
 from sklearn.impute import SimpleImputer
-from sklearn.model_selection import train_test_split
 
-def preprocess_data(data, target_column, impute_method, save_path):
-    # Pemisahan Fitur dan Target
-    X = data.drop(columns=[target_column])
-    y = data[target_column]
-    
-    # Pembagian Data Latih
-    X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.25, stratify=y, random_state=42)
-
+def preprocess_data(data, impute_method, save_path, output_path):
     # Pipeline imputasi (penanganan missing values)
     cleaning = Pipeline(steps=[
         ('imputer', SimpleImputer(strategy=impute_method))
     ])
 
     # Fit pipeline untuk memproses data
-    X_train = cleaning.fit_transform(X_train)
-    X_test = cleaning.transform(X_test)
+    data_clean = cleaning.fit_transform(data)
 
     # Simpan pipeline
     dump(cleaning, save_path) # save path untuk menyimpan preprocessor
 
-    return X_train, X_test, y_train, y_test
+    # Simpan data hasil preprocessing
+    cleaned_data = pd.DataFrame(data_clean, columns=data.columns)
+    cleaned_data_path = output_path
+    cleaned_data.to_csv(cleaned_data_path, index=False)
+
+    return cleaned_data, cleaned_data_path
