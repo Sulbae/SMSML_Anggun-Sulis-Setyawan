da91ef5a312e63d266bc68454b14acc8165bffb3
Sulbae <assulis500@gmail.com>
2025-06-28 15:06:59 +0700

    add dataset artifact command

--- Eksperimen_SML_Anggun-Sulis-Setyawan/MLproject/automate_preprocessing.py
+++ Eksperimen_SML_Anggun-Sulis-Setyawan/MLproject/automate_preprocessing.py
@@ -71,3 +71,4 @@ if __name__ == "__main__":
 
         # Log preprocessor
         mlflow.log_artifact(args.save_path, artifact_path="preprocessor")
+        mlflow.log_artifact(dataset_path, artifact_path="dataset")
