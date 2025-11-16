2cbab87c070542859abfada7980513df09c7d6f6
Sulbae <assulis500@gmail.com>
2025-11-16 17:30:27 +0700

    update modelling.py

--- Membangun_model/modelling.py
+++ Membangun_model/modelling.py
@@ -9,7 +9,7 @@ from joblib import dump
 
 # Konfigurasi
 DATASET_PATH = "water_potability_preprocessing.csv"
-N_ESTIMATOR = 100
+N_ESTIMATORS = 100
 MAX_DEPTH = 5
 
 # Konfigurasi MLFLow
@@ -23,25 +23,21 @@ mlflow.autolog(log_models=False, input_example=True)
 data = pd.read_csv(DATASET_PATH)
 
 # Split data
+X = data.drop(columns=['Potability'])
+y = data['Potability']
+
 X_train, X_test, y_train, y_test = train_test_split(
-    data.drop(columns=['Potability'], axis=1), 
-    data['Potability'], 
-    test_size=0.25, 
-    random_state=42)
+    X, y, test_size=0.25, random_state=42)
 
 # Menyimpan snippet atau input sample
 input_example = X_train.iloc[0:5]
 
 # Modelling
 with mlflow.start_run():
-    
-    # Log parameters
-    mlflow.log_param("n_estimator", N_ESTIMATOR)
-    mlflow.log_param("max_depth", MAX_DEPTH)
 
     # Train model
     model = RandomForestClassifier(
-        n_estimators=N_ESTIMATOR,
+        n_estimators=N_ESTIMATORS,
         max_depth=MAX_DEPTH
     )
     model.fit(X_train, y_train)
@@ -49,18 +45,7 @@ with mlflow.start_run():
     # Log metrics
     accuracy = model.score(X_test, y_test)
     mlflow.log_metric("accuracy", accuracy)
-    print(f"Accuracy: {accuracy:.4f}")
 
     # Simpan model ke file lokal
     model_path = "rf_model_v1.joblib"
     dump(model, model_path)
-
-    # Log file model sebagai artefak ke MLflow
-    mlflow.log_artifact(model_path, artifact_path="model_artifacts")
-
-    # Log model setelah selesai
-    mlflow.sklearn.log_model(
-        sk_model=model,
-        artifact_path="model",
-        input_example=input_example
-    )
