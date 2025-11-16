6ea91710cf8ea31a1d635ac6a03d6a3b1dea2064
Sulbae <assulis500@gmail.com>
2025-11-16 17:46:20 +0700

    update modellingopt.py

--- Membangun_model/modelling_tuning.py
+++ Membangun_model/modelling_tuning.py
@@ -1,101 +1,89 @@
-import numpy as np
-import pandas as pd
-from sklearn.model_selection import train_test_split, ParameterGrid
-from sklearn.ensemble import RandomForestClassifier
-from sklearn.metrics import accuracy_score, precision_score, recall_score, f1_score
-import random
 import mlflow
-import os
-from joblib import dump
+import dagshub
+import pandas as pd
+from sklearn.ensemble import RandomForestClassifier
+from sklearn.model_selection import train_test_split
+from sklearn.metrics import precision_score, recall_score, f1_score
+import random
+import numpy as np
 
-# Konfigurasi MLFLow
-mlflow.set_tracking_uri("http://127.0.0.1:5000")
+# Konfigurasi 
+DATASET_PATH = "water_potability_preprocessing.csv"
+N_ESTIMATORS_RANGE = np.linspace(10, 100, 3, dtype=int)
+MAX_DEPTH_RANGE = np.linspace(1, 50, 3, dtype=int)
+TEST_SIZE = 0.25
+
+# Set Tracking melalui DagsHub
+dagshub.init(
+    repo_owner="Sulbae",
+    repo_name="Latihan-MLFlow",
+    mlflow=True
+)
+# Set Nama Eksperimen
 mlflow.set_experiment("Modelling Random Forest with Grid Search")
 
-# Set 2 seed untuk reproducibility
-random.seed(42)
-np.random.seed(42)
-
 # Load dataset
-dataset_file = "water_potability_preprocessing.csv"
-dataset_path = os.path.abspath(dataset_file)
-dataset_version = "v1.0"
-
-data = pd.read_csv(dataset_file)
+data = pd.read_csv(DATASET_PATH)
 
 # Split data
 X = data.drop(columns=['Potability'], axis=1)
 y = data['Potability']
 
-X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.25, random_state=42)
+X_train, X_test, y_train, y_test = train_test_split(
+    X, y, test_size=TEST_SIZE, 
+    random_state=42)
 
 # Menyimpan snippet atau input sample
-input_example = X_train.iloc[:5]
+input_example = X_train.iloc[0:5]
 
-# Parameter model
-param_grid = {
-    "n_estimators": [50, 100, 150],
-    "max_depth": [5, 10, 20],
-    "min_samples_split": [2, 5, 10]
-}
-
-# Tracking best model
-best_accuracy = 0
-best_params = None
+best_acc = 0
+best_params = {}
 best_model = None
-best_precision = None
-best_recall = None
 
-# Modelling
-with mlflow.start_run(run_name="Grid Search Experiment"):
-    for params in ParameterGrid(param_grid):
+for n_estimators in N_ESTIMATORS_RANGE:
+    for max_depth in MAX_DEPTH_RANGE:
+        with mlflow.start_run(run_name=f"grid_search_{n_estimators}_{max_depth}") as run:
 
-        run_name = "params_" + "_".join(f"{k}{v}" for k, v in params.items())
+            # Log Parameter
+            mlflow.log_param("n_estimators", n_estimators)
+            mlflow.log_param("max_depth", max_depth)
+            mlflow.log_param("test_size", TEST_SIZE)
 
-        with mlflow.start_run(nested=True, run_name=run_name):
             # Train model
             model = RandomForestClassifier(
-                **params,
+                n_estimators=n_estimators, 
+                max_depth=max_depth, 
                 random_state=42
             )
             model.fit(X_train, y_train)
 
-            # Predict
+            # Evaluate model
             y_pred = model.predict(X_test)
             
-            # Accuracy
-            accuracy = accuracy_score(y_test, y_pred)
-            precision = precision_score(y_test, y_pred)
-            recall = recall_score(y_test, y_pred)
-            
-            # Log parameters
-            mlflow.log_params(params)
+            accuracy = model.score(X_test, y_test)
+            precision = precision_score(y_test, y_pred, average="weighted")
+            recall = recall_score(y_test, y_pred, average="weighted")
+            f1 = f1_score(y_test, y_pred, average="weighted")
+
+            # Log Metrics
             mlflow.log_metric("accuracy", accuracy)
             mlflow.log_metric("precision", precision)
             mlflow.log_metric("recall", recall)
+            mlflow.log_metric("f1_score", f1)
 
-            # Track best model
-            if accuracy > best_accuracy:
-                best_accuracy = accuracy
-                best_params = params
+            # Save the best model
+            if accuracy > best_acc:
+                best_acc = accuracy
+                best_params = {
+                    "n_estimators": n_estimators, 
+                    "max_depth": max_depth
+                }
                 best_model = model
-                best_precision = precision
-                best_recall = recall
-    
-    # Log ke parent run
-    mlflow.log_params({f"best_{k}": v for k, v in best_params.items()})
-    mlflow.log_metrics("best_accuracy", best_accuracy)
-    mlflow.log_metric("best_precision", best_precision)
-    mlflow.log_metric("best_recall", best_recall)
-    
-    # Simpan model ke file lokal
-    best_model_path = "best_rf_model.joblib"
-    dump(best_model, best_model_path)
 
-    # Log file model sebagai artefak ke MLflow
-    mlflow.log_artifact(best_model_path, artifact_path="best_model_artifacts")
+with mlflow.start_run(run_name="best_model_run"):
+    mlflow.log_params(best_params)
+    mlflow.log_metric("best_accuracy", best_acc)
 
-    # Log model setelah selesai
     mlflow.sklearn.log_model(
         sk_model=best_model,
         artifact_path="best_model",
