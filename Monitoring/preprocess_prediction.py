88771f813116d56d590925c74146d52f51122724
Sulbae <assulis500@gmail.com>
2025-11-22 14:52:29 +0700

    update

--- Monitoring/preprocess_prediction.py
+++ Monitoring/preprocess_prediction.py
@@ -6,7 +6,7 @@ import pandas as pd
 import requests
 import json
 
-def prediction_response(input=input):
+def preprocess_to_predict(input=input):
     preprocess_pipeline = joblib.load("preprocessing_pipeline.joblib")
 
     def data_preprocessing(input):
@@ -17,14 +17,14 @@ def prediction_response(input=input):
 
         return df
 
-    def prediction(df):
+    def prediction(data):
 
         url = "http://127.0.0.1:5002/invocations"
         headers = {"Content-Type": "application/json"}
-        response = requests.post(url, data=df, headers=headers)
+        response = requests.post(url, data=data, headers=headers)
         response = response.json().get("predictions")
         
         return response
 
 if __name__ == "__main__":
-    prediction_response(input=input)
+    preprocess_to_predict(input=input)
